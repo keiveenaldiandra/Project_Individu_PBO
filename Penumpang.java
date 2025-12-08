6f99488217f76d4f69d50ea55995655d8f6efcbf
keiveenaldiandra <keiveenaldiandra23utama@gmail.com>
2025-12-09 00:36:02 +0700

    Update Penumpang.java

--- Penumpang.java
+++ Penumpang.java
@@ -27,3 +27,34 @@ public class Penumpang extends Orang implements Payable {
             this.saldo += saldobaru;
         }
     }
+
+// Overloading buat tambahSaldo sebagai kreativitas
+    public void tambahSaldo(int saldobaru, boolean bonus) {
+        if (bonus) {
+            this.saldo += saldobaru + 500; // Bonus 500
+        } else {
+            tambahSaldo(saldobaru);
+        }
+    }
+
+    @Override
+    public void bayar(int ongkos) throws SaldoTidakCukupException {
+        if (saldo < ongkos) {
+            throw new SaldoTidakCukupException("Saldo tidak cukup untuk membayar ongkos sebesar " + ongkos + ". Saldo saat ini: " + saldo);
+        }
+        saldo -= ongkos;
+    }
+
+    @Override
+    public PrioritasType getPrioritasType() {
+        if (umur > 60) return PrioritasType.LANSIA;
+        if (umur < 10) return PrioritasType.ANAK;
+        if (hamil) return PrioritasType.HAMIL;
+        return PrioritasType.BIASA;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Nama: " + nama + ", Umur: " + umur + ", Hamil: " + hamil + ", Saldo: " + saldo;
+    }
+}
